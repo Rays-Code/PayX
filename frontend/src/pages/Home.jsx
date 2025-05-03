edd3b0508ec28544d213bd55886f800e96764e60
Dipraj Ray <raydipraj1234@gmail.com>
2025-05-03 23:53:00 +0530

    fixed bugs

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -21,9 +21,9 @@ const Home = () => {
       {/* Centered Text */}
       <div className="flex h-full w-full items-center justify-center">
         <div className="flex flex-col text-white text-5xl lg:text-8xl font-poppins font-semibold text-center drop-shadow-xl">
-          <div className="lg:p-2">Payments</div>
-          <div className="lg:p-2">made effortless, secure</div>
-          <div className="lg:p-2">lightning fast.</div>
+          <div className="p-1 lg:p-2">Payments</div>
+          <div className="p-1 lg:p-2">made effortless, secure</div>
+          <div className="p-1 lg:p-2">lightning fast.</div>
         </div>
       </div>
     </div>
